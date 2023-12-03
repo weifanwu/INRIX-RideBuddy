c3062d3fa1eb5605e6036762804cab907d5f57ca
weifan_certik <weifan.wu@certik.com>
2023-12-03 07:49:38 -0800

    update

--- client/src/components/AddPost.js
+++ client/src/components/AddPost.js
@@ -111,16 +111,29 @@ function App(props) {
   }
 
   async function showRoute() {
-    const points = await fetch("");
+    const start = originRef.current.value;
+    const end = destiantionRef.current.value;
+    const startData = await getGeocode(start);
+    const endDate = await getGeocode(end);
+    const origin = startData[0] + "," + startData[1];
+    const destination = endDate[0] + "," + endDate[1];
+    const response = await fetch("http://127.0.0.1:5000/findRoute?point1=" + origin + "&point2=" + destination);
+    const information = await response.json();
+    const points = information["information"]
+    console.log("information points:");
+    console.log(points);
     // eslint-disable-next-line no-undef
-    const directionsService = new google.maps.DirectionsService()
-    const results = await directionsService.route({
-      origin: originRef.current.value,
-      destination: destiantionRef.current.value,
+    let wps = [];
+    for (let i = 0; i < points.length; i++) {
+      console.log("coodnats here: ");
+      console.log(points[i][0]);
+      console.log(points[i][1]);
       // eslint-disable-next-line no-undef
-      travelMode: google.maps.TravelMode.DRIVING,
-    })
-    props.setDirections(results)
+      const current = new google.maps.LatLng(points[i][1], points[i][0]);
+      wps.push(current);
+    }
+
+    props.setPoints(wps)
     navigate("/map");
   }
 
--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from "react";
-import { useJsApiLoader, GoogleMap, Marker, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
+import { useJsApiLoader, GoogleMap, Marker, Polyline, Autocomplete, DirectionsRenderer } from '@react-google-maps/api'
 import { useLocation } from 'react-router-dom';
 import { InfoWindow } from "@react-google-maps/api";
 import Card from '@mui/material/Card';
@@ -15,7 +15,15 @@ import { useNavigate } from "react-router-dom";
 import FilterSidebar from "./filter";
 
 export default function Map(props) {
-    const center = { lat: 47.625168, lng: -122.337751 };
+
+
+
+    const path = props.points;
+
+
+
+
+    const center = path[0];
     const navigate = useNavigate();
 
     const [map, setMap] = useState(/** @type google.maps.Map */ (null))
@@ -66,7 +74,6 @@ export default function Map(props) {
     // }
 
     useEffect(() => {
-      console.log("test Get Post Data!");
       let allPosts = [];
 
       fetch(`/testGetPost`, {
@@ -196,6 +203,12 @@ export default function Map(props) {
         />
       )))
       }
+
+      <Polyline
+              path={path}
+              strokeColor="#FF0000"
+              strokeOpacity={0.8}
+              strokeWeight={2} />
 
       {open && (
         <InfoWindow position={start} onCloseClick={() => setOpen(false)}>
